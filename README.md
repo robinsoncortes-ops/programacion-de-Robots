268d2a1b72aaaeecb4a3bef428b969602b328bb4
Robinson alejandro cortes salinas <robinsoncortes@colegioguanenta.edu.co>
2026-04-24 09:44:43 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # programacion-de-Robots
 Crea un programa en Python, usando POO, que simule el siguiente juego
+
+
+![alt text](6a263667-3fe1-4a9c-af72-d323e2f04cfa.jpg)
---
+++ taller 1/ejercicio 1.py
@@ -0,0 +1,12 @@
+
+# clase de robot
+
+# Atributos para el robots de Ataque 
+
+import random
+
+class Robot:
+    def __init__(self, Nombre, Bateria, Escudo):
+        self._Nombre = "perez"
+        self._Bateria = "45%"
+        self._Escudo = "100"
